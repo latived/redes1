2df98a19b99a3e05685ed4f5137fa62fb630b094
vitalwarley <wvb@ic.ufal.br>
2018-05-14 23:33:15 -0300

    add parse_cmd to begin to  parse the given command

--- client.py
+++ client.py
@@ -8,13 +8,17 @@ def main(host='localhost', port=12345):
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
         soc.connect((HOST, PORT))
+        cmd_input = ""
+        while cmd_input != "QUIT":
+            cmd_input = input("COMMAND?\n")
+            soc.sendall(cmd_input.encode("utf8"))
+            result_bytes = soc.recv(4096)
+            result_string = result_bytes.decode("utf8")
 
-        clients_input = input("Tell me what \n")
-        soc.sendall(clients_input.encode("utf8"))
-        result_bytes = soc.recv(4096)
-        result_string = result_bytes.decode("utf8")
+            print("ANSWER: {}".format(result_string))
 
-        print("Result from server is {}".format(result_string))
+        print("Quiting game...\n")
+
 
 if __name__ == '__main__':
     main()
--- client_thread.py
+++ client_thread.py
@@ -2,19 +2,30 @@
 
 import socket
 
-def do_some_stuff(input_string):
-    print("Processing that input!")
-    return input_string[::-1]
+valid_commands = ["CREATE", "ADD", "REMOVE", "START", "FOCUS", "CONF", 
+        "PLAY", "SHOW", "LIST", "HELP", "EXIT", "QUIT"]
+
+# Parse command and return a instance of the desired command
+def parse_cmd(cmd_full):
+    # Split by spaces to get COMMAND <option(s)>=<value>
+    cmd = cmd_full.split(' ')
+
+    if cmd[0] not in valid_commands:
+        msg = "Invalid command!"
+        raise SyntaxError(msg)
+    
+    return getattr(Command, cmd[0], cmd[1:])
 
 def client_thread(conn, host, port, MAX_BUFFER_SIZE = 4096):
     with conn:
-        input_bytes = conn.recv(MAX_BUFFER_SIZE)
+        while True:
+            command = conn.recv(MAX_BUFFER_SIZE)
+            if not command: break # TODO: add useful msg
+            command_clean = command.decode("utf8").rstrip()
 
-        input_fc = input_bytes.decode("utf8").rstrip()
-        res = do_some_stuff(input_fc)
-        print("Result of processing {} is: {}".format(input_fc, res))
-
-        vysl = res.encode("utf8")
-        conn.sendall(vysl)
+            # Parse command
+            msg_return = parse_cmd(command_clean).encode("utf8")
+            conn.sendall(msg_return)
+        
         print("Connection " + host + ":" + port + " ended")
 
